c2cca37c04e3fd446b6296d52366c2637418c461
forrestriley <93394555+forrestriley@users.noreply.github.com>
2024-06-14 15:51:05 -0400

    Triangle Calc Basic

--- Projects/triangle_calc/triangle_calc.py
+++ Projects/triangle_calc/triangle_calc.py
@@ -1,63 +1,39 @@
 '''
 Triangle Calculator
-
-- Area
-- Perimeter
 '''
-# Using tkinter fo GUI
-import tkinter as tk
 
-# Initializes window
-window = tk.Tk()
+'''
+Write a program that will calculate the area of a triangle, below is the equation needed to solve this problem
 
-# Set Size of Window
-window.geometry("800x600")
+Area of Triangle= 1/2 x base x height
+For your example, base = 20, height = 15
+Requirements
+-Comment your code
+-use proper naming for your variables
+-Include a string within your print built in function that says, "The area of your triangle is " as output (formatted string)
+'''
 
-# Set Title of Window
-window.title("Forrest's Triangle Calculator")
+# initiates base as user's input
+base = input("Enter Triangle's Base Length   ")
 
-# Top Text
-label = tk.Label(master=window, text="Welcome to the Triangle Zone", font=('Arial', 18))
-label.pack(padx=10, pady=10)
+while ( base.isdigit() != True ):# checks if the input was a valid number, if False it runs the loop
+  base = input("Enter Triangle's Base Length   ")# re-assigns the value of base to the new user input
+  if base.isdigit() == True:# if they entered a number it breaks the loop, otherwise the loop continues to ask for valid input
+    break
 
-# Initialize Grid 3x3
-frame = tk.Frame(window)
-frame.columnconfigure(0,weight=1)
-frame.columnconfigure(1,weight=1)
-frame.columnconfigure(2,weight=1)
+# initiates height as user's input
+height = input("Enter Triangle's Height   ")
 
-# Side Size Entry Boxes
-side_one = tk.Entry(frame)
-side_one.grid(row=1, column=0, padx=10, pady=10)
+while ( height.isdigit() != True):# checks if the input was a valid character, if False it runs the loop
+  height = input("Enter Triangle's Height   ")# re-assigns the value of height to the new user input
+  if height.isdigit() == True:# if they entered a character it breaks the loop, otherwise the loop continues to ask for valid input
+    break
 
-side_two = tk.Entry(frame)
-side_two.grid(row=1, column=1, padx=10, pady=10)
+# sets the variable to 1/2 x base x height | base and height are made integers from strings via built in function
+area_of_triangle = (1/2 * int(base) * int(height))
 
-side_three = tk.Entry(frame)
-side_three.grid(row=1, column=2, padx=10, pady=10)
-
-# Buttons
-btn1 = tk.Button(frame, text="Perimeter", font=('Arial', 12))
-btn1.grid(row=2, column=0, padx=10, pady=10)
-btn2 = tk.Button(frame, text="Area", font=('Arial', 12))
-btn2.grid(row=2, column=2, padx=10, pady=10)
-
-frame.pack()
-
-
-
-
-
-# Starts the Program
-window.mainloop()
-
-
-
-
-
-
-
-
+#  Prints the text with formated string to put the variable directly into the str
+print(f"The area of your triangle is {area_of_triangle}.")
 
 
 
