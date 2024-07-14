83978d6d677a34c3b292104ebf4fc619afc6282c
forrestriley <93394555+forrestriley@users.noreply.github.com>
2024-07-13 21:03:31 -0400

    Email and password checks complete, login functions added

--- Projects/project_one/project_one.py
+++ Projects/project_one/project_one.py
@@ -2,9 +2,10 @@
 
 email = ""
 password=""
+global successful_login
 successful_login = False
-email_check = False
-password_check = False
+global email_check 
+global password_check
 # while loop checking for a successful login
 
 while successful_login == False:
@@ -25,22 +26,49 @@ while successful_login == False:
         email_test_one = email_split[0].isalnum()#checks username for alphanumeric content and saves it to variable
 
         if email_test_one == True: #if test 1 is true execute
-            email_test_two = email_split[1].find(".")
+            email_test_two = email_split[1].find(".", -5,-3)
+
 
             if email_test_two == -4:
-                email_test_three_variable = email_split[1].isalnum()
-                email_test_three = 
+                email_test_three = email_split[1].find(".", -5,-3)
 
+                if email_test_three == True:
+                    email_check = True
+                    print("A valid password is alphanumeric and my contain special characters")
+                    password = input("Enter your password")
+                    password_test = password.isascii()
+
+                    if password_test == True:
+                        print("The email and password are valid, login to finish account creation")
+                        password_check = True
+
+                        if email_check == True and password_check == True:
+                            print("Enter your email")
+                            email_login = input()
+                            if email == email_login:
+                                print("Enter your password")
+                                password_login = input()
+                                if password == password_login:
+                                    print("Account successfully created, and you have been logged in")
+
+                                else:#if test one is false execute
+                                    print("Incorrect password, please try creating an account again")#print error message
+                                    break#breaks loop
+                            else:#if test one is false execute
+                                print("Incorrect email, please try creating an account again")#print error message
+                                break#breaks loop
+                    else:#if test one is false execute
+                        print("Invalid password")#print error message
+                        break#breaks loop
+                else:#if test one is false execute
+                    print("No period detected before the three letter domain")#print error message
+                    break#breaks loop
             else:#if test one is false execute
                 print("No period detected before the three letter domain")#print error message
                 break#breaks loop
-
-
         else:#if test one is false execute
             print("Invalid username for email")#print error message
             break#breaks loop
-
-
     else: #if no @ is found execute
         print("No @ found in the email, please try again.") #prints message for missing @
         break #breaks loop
