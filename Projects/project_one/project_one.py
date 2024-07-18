415a255d8400373a8f6400b14b91eb6bf7f10dbc
forrestriley <93394555+forrestriley@users.noreply.github.com>
2024-07-18 18:43:03 -0400

    Project finished and commented

--- Projects/project_one/project_one.py
+++ Projects/project_one/project_one.py
@@ -12,66 +12,63 @@ while successful_login == False:
 
     # print prompt stating email requirements tom@
 
-    print("Emails should have valid valid username followed by an @, a mail server between 3 and 7 letters long, and end with a '.' before a three letter domain")
+    print("Emails should have valid valid username followed by an @, a mail server between 3 and 7 letters long, and end with a '.' before a 3 to 5 letter domain")
 
     #get input from user
 
-    email = str(input("Please enter your email"))
+    email = str(input("Please enter your email "))
 
     #check for valid email
     email = email.strip()#strips whitespace around input
 
+
+
     if email.find("@") > -1: #checks for  @ if found, execute
         email_split = email.split("@")#splits input at the @
         email_test_one = email_split[0].isalnum()#checks username for alphanumeric content and saves it to variable
+         
 
         if email_test_one == True: #if test 1 is true execute
-            email_test_two = email_split[1].find(".", -5,-3)
+            email_test_two = email_split[1].find(".", -5,-2) #checks for a "." between the fifth and third to last characters in 
 
+            if email_test_two != -1: #if find is able to locate a '.' in the correct area
+                    email_check = True #sets email check to true
+                    print("A valid password is alphanumeric and my contain special characters") #tells the password rules
+                    password = input("Enter your password ") #gets user input for password
+                    password_test = password.isascii() #checks for numbers, letters, special characters
 
-            if email_test_two == -4:
-                email_test_three = email_split[1].find(".", -5,-3)
+                    if password_test == True: #if password contains valid input
+                        print("The email and password are valid, login to finish account creation") #prints that the password is valid
+                        password_check = True #sets password check to true
 
-                if email_test_three == True:
-                    email_check = True
-                    print("A valid password is alphanumeric and my contain special characters")
-                    password = input("Enter your password")
-                    password_test = password.isascii()
-
-                    if password_test == True:
-                        print("The email and password are valid, login to finish account creation")
-                        password_check = True
-
-                        if email_check == True and password_check == True:
-                            print("Enter your email")
-                            email_login = input()
-                            if email == email_login:
-                                print("Enter your password")
-                                password_login = input()
+                        if email_check == True and password_check == True: #if email and password checks are ture
+                            print("Enter your email") #asks for email
+                            email_login = input() # gets input from user
+                            if email == email_login: #if entered email is the same as saved email
+                                print("Enter your password") #asks for password
+                                password_login = input() #gets user input 
                                 if password == password_login:
-                                    print("Account successfully created, and you have been logged in")
+                                    print("Account successfully created, and you have been logged in") #prints confirmation message, and login message
+                                    break #breaks while loop
 
-                                else:#if test one is false execute
+                                else:#if password != the previous password
                                     print("Incorrect password, please try creating an account again")#print error message
-                                    break#breaks loop
-                            else:#if test one is false execute
+                                    
+                            else:#if email =! the previous email
                                 print("Incorrect email, please try creating an account again")#print error message
-                                break#breaks loop
-                    else:#if test one is false execute
-                        print("Invalid password")#print error message
-                        break#breaks loop
-                else:#if test one is false execute
-                    print("No period detected before the three letter domain")#print error message
-                    break#breaks loop
-            else:#if test one is false execute
-                print("No period detected before the three letter domain")#print error message
-                break#breaks loop
-        else:#if test one is false execute
+                                
+                        else:#if ascii check is false
+                            print("Invalid password")#print error message
+                            
+            else:#if '.' is not detected in range
+                print("No period detected before the domain")#print error message
+                
+        else:#if there is no valid input before @
             print("Invalid username for email")#print error message
-            break#breaks loop
+            
     else: #if no @ is found execute
         print("No @ found in the email, please try again.") #prints message for missing @
-        break #breaks loop
+        
 
 
     #print prompt stating password requirements
