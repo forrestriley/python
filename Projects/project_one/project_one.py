f705fd88ae3be96b3cd2d686b1b24e103185f9fd
forrestriley <93394555+forrestriley@users.noreply.github.com>
2024-07-18 22:50:08 -0400

    Hopefully finished

--- Projects/project_one/project_one.py
+++ Projects/project_one/project_one.py
@@ -11,7 +11,6 @@ global email_check
 email_check = False
 global password_check
 password_check = False
-password_regex = r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!?@#$^&*_-])$"
 # while loop checking for a successful login
 
 while successful_login == False:
@@ -38,52 +37,51 @@ while successful_login == False:
             email_test_two = email_split[1].find(".", -5,-2) #checks for a "." between the fifth and third to last characters in 
 
             if email_test_two != -1: #if find is able to locate a '.' in the correct area
-                email_check = True #sets email check to true
-                while password_check == False:
-                    print("A valid password is must be at least 8 characters long with no spaces, contain at least one uppercase letter, one lowercase letter, one number, and at least one of the following: !, ?, @, #, $, ^, &, _, -") #tells the password rules
-                    password = input("Enter your password ") #gets user input for password
-                    length = len(password) #sets length to pass length
-                    if length >= 8: #checks for password length being 8 or more
+                    email_check = True #sets email check to true
+                    if password_check == False:
+                        print("A valid password is must be at least 8 characters long with no spaces, contain at least one uppercase letter, one lowercase letter, one number, and at least one of the following: !, ?, @, #, $, ^, &, _, -") #tells the password rules
+                        password = input("Enter your password ") #gets user input for password
+                        length = len(password) #sets length to pass length
+                        if length >= 8: #checks for password length being 8 or more
                             
-                        pass_test_one = password.find(" ")#checks for whitespace, returns -1 if none were detected
+                            pass_test_one = password.find(" ")#checks for whitespace, returns -1 if none were detected
 
-                        if pass_test_one == -1:
-                            if pass_test_one == -1 and length >= 8:
-                                if pass_test_one == -1:#if no whitespace found
-                                    if not re.search("[a-z]", password): #checks for at least one lower case letter
-                                        print("No lower case letters found") # if fails print designated error
-                                    if not re.search("[A-Z]", password): #checks for at least one capitol letter
-                                        print("No upper case letters found") # if fails print designated error
-                                    if not re.search("[0-9]", password): #checks for at least one digit
-                                        print("No number detected", password) # if fails print designated error
-                                    if not re.search("[!?@#$^&*_-]", password): #checks for at least one of the specified characters
-                                        print("Must include at least one !, ?, @, #, $, ^, &, _, or -") # if fails print designated error
-                                else:
-                                    password_check = True
-                                    print("Password accepted")
-                                    break
+                            if pass_test_one == -1:
+                                        
+                                        if not re.search("[a-z]", password): #checks for at least one lower case letter
+                                            print("No lower case letters found") # if fails print designated error
+                                        elif not re.search("[A-Z]", password): #checks for at least one capitol letter
+                                            print("No upper case letters found") # if fails print designated error
+                                        elif not re.search("[0-9]", password): #checks for at least one digit
+                                            print("No number detected", password) # if fails print designated error
+                                        elif not re.search("[!?@#$^&*_-]", password): #checks for at least one of the specified characters
+                                            print("Must include at least one !, ?, @, #, $, ^, &, _, or -") # if fails print designated error
+                                        else:
+                                            password_check = True
+                                            print("Password Accepted")
+
+                                            if email_check == True and password_check == True: #if email true
+                                                print("Enter your email") #asks for email
+                                                email_login = input() # gets input from user
+                                                if email == email_login: #if entered email is the same as saved email
+                                                    print("Enter your password") #asks for password
+                                                    password_login = input() #gets user input 
+                                                    if password == password_login:
+                                                        print("Account successfully created, and you have been logged in") #prints confirmation message, and login message
+                                                        break #breaks while loop
+
+                                                    else:
+                                                        print("Incorrect Password, please try creating an account again")#prints failure message
+                                                else:#if email =! the previous email
+                                                    print("Incorrect email, please try creating an account again")#print error message
                             else:
                                 print("Invalid password, spaces were detected")
                         else: #if the password is too short
-                            print(f"The password was not long enough, it was only {length} characters long") #prints out error, and length of entered password          
-                if email_check == True and password_check == True: #if email and password checks are ture
-                        print("Enter your email") #asks for email
-                        email_login = input() # gets input from user
-                        if email == email_login: #if entered email is the same as saved email
-                            print("Enter your password") #asks for password
-                            password_login = input() #gets user input 
-                            if password == password_login:
-                                print("Account successfully created, and you have been logged in") #prints confirmation message, and login message
-                                break #breaks while loop
-                                                
-                        else:#if email =! the previous email
-                            print("Incorrect email, please try creating an account again")#print error message                           
+                            print(f"The password was not long enough, it was only {length} characters long") #prints out error, and length of entered password                                       
             else:#if '.' is not detected in range
                 print("No period detected before the domain")#print error message
-                
         else:#if there is no valid input before @
             print("Invalid username for email")#print error message
-            
     else: #if no @ is found execute
         print("No @ found in the email, please try again.") #prints message for missing @
         
